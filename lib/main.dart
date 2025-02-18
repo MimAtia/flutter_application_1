78205cdf923d191892386d9215c4697b8aecb8f2
MimAtia <mim15-5375@diu.edu.bd>
2025-02-18 20:09:52 +0600

    task1

--- lib/main.dart
+++ lib/main.dart
@@ -10,70 +10,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fancy Icon App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Shoe widget'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      title: "Fancy Icon App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fancy Icon App"),
+          backgroundColor: Colors.blue[400],
+          centerTitle: true,
+        ),
+        body: Row(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Text(
-                  '50% OFF',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Icon(Icons.favorite),
-              ],
+            Text(
+              "Hi,",
+              style:TextStyle(
+                color: Colors.red,
+                fontSize: 30,
+              ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Nike Air Shoes',
-              style: TextStyle(fontSize: 18),
+            Text(
+              "This is",
+              style:TextStyle(
+                color: Colors.yellow,
+                fontSize: 50,
+              ),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Text(
-                  '\$45,000',
-                  style: TextStyle(fontSize: 16),
-                ),
-                Text(
-                  '\$55,000',
-                  style: TextStyle(
-                    fontSize: 16,
-                    decoration: TextDecoration.lineThrough,
-                  ),
-                ),
-              ],
+            Text(
+              "Flutter",
+              style:TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
             ),
           ],
         ),
-      ),
+
+
+        ),
+        
+      
     );
   }
 }
+
